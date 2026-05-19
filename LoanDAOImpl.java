5610f770626220ac86f367c5e161512fc37d5c04
I_Like_Cyberpunk <gtaeho85@gmail.com>
2026-05-19 15:43:21 +0900

    j

--- LoanDAOImpl.java
+++ LoanDAOImpl.java
@@ -10,6 +10,15 @@ public class LoanDAOImpl implements LoanDAO {
             st = con.prepareStatement(sql);
             st.setInt(1,bookId);
             st.setInt(2, memberId);
+            st.executeUpdate(); 
+            //executeQuery는 select 할떄
+            //executeUpdate는 ddl할때 ex) insert delelte update ..
+
+            //대출화면
+            PreparedStatement st2 = con.prepareStatement("UPDATE BOOK SET IS_AVAILABLE = 'N' WHERE BOOK_ID = ?");
+            st2.setInt(1, bookId);
+            st2.executeUpdate();
+            st2.close();
         }
         catch(Exception e){
             System.out.println(e);
@@ -20,9 +29,30 @@ public class LoanDAOImpl implements LoanDAO {
         System.out.println("멤버 id = "+memberId+", 책 id = "+bookId+"대출등록 성공");
     }
     public void returnBook(int loanId, int bookId){
-        
-    }
-    public void getOverdueList(){
+        Connection con = DBConnection.getConnection();
+        PreparedStatement st = null;
+        String sql = "UPDATE LOAN SET RETURN_DATE WHERE LOAN_ID = ?";
+        try{
+            st = con.prepareStatement(sql);
+            st.setInt(1, loanId);
+            st.executeUpdate();
 
+            PreparedStatement st2 = con.prepareStatement("UPDATE BOOK SET IS_AVAILABLE = Y WHERE BOOK_ID = ?");
+            st2.setInt(1, bookId);
+            st2.executeUpdate();
+            st2.close();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        finally{
+            DBConnection.close(con, st, null);
+        }
+        System.out.println("반납완료 (loan ID : " + loanId + "Book ID : " + bookId);
+    }
+
+    //연체조회
+    public void getOverdueList(){
+        
     }
 }
